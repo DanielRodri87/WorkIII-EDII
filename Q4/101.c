b7ca2148129c3b2371b56ffc0112a798014064c6
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2025-01-07 21:55:50 -0300

    Q4 organizada

--- Q4/101.c
+++ Q4/101.c
@@ -14,153 +14,28 @@ typedef struct Employee
     float salary;
 } Employee;
 
-Employee generateEmployee()
-{
-    int number = rand() % 100000;
-    Employee employee;
-    employee.salary = (float)number;
-
-    for (int i = 0; i < REGISTRATION_SIZE; i++, number /= 10)
-    {
-        int currentDigit = number % 10;
-        employee.registration[i] = (char)currentDigit + '0';
-        employee.name[i] = (char)currentDigit + '0';
-        employee.role[i] = (char)currentDigit + '0';
-    }
-
-    employee.registration[REGISTRATION_SIZE - 1] = '\0';
-    employee.name[REGISTRATION_SIZE - 1] = '\0';
-    employee.role[REGISTRATION_SIZE - 1] = '\0';
-
-    if (employee.registration[0] == '0')
-        employee.registration[0] = '1';
-
-    return (employee);
-}
-
 typedef struct hashTable
 {
     int hash[AVAILABLE_HASH_SIZE];
     Employee employee[AVAILABLE_HASH_SIZE];
 } HashTable;
 
-void initializeHashTable(HashTable *hashTable)
-{
-    for (int i = 0; i < AVAILABLE_HASH_SIZE; i++)
-        hashTable->hash[i] = -1;
-}
-
-int charToInt(char c)
-{
-    return ((int)c - '0');
-}
-
-int functionHash1(char registration[])
-{
-    char rearranged[REGISTRATION_SIZE];
-
-    rearranged[0] = registration[REGISTRATION_SIZE - 2];
-    rearranged[1] = registration[REGISTRATION_SIZE - 1];
-    for (int i = 0, j = 2; i < REGISTRATION_SIZE - 2; i++, j++)
-        rearranged[j] = registration[i];
-
-    int integerValue = atoi((char[]){rearranged[1], rearranged[3], rearranged[5]});
-
-    return (integerValue % AVAILABLE_HASH_SIZE);
-}
-
-int functionHash2(char registration[])
-{
-    int part1 = (charToInt(registration[0]) * 100) + (charToInt(registration[2]) * 10) + charToInt(registration[5]);
-    int part2 = (charToInt(registration[1]) * 100) + (charToInt(registration[3]) * 10) + charToInt(registration[4]);
-
-    int sumParts = part1 + part2;
-
-    return (sumParts % AVAILABLE_HASH_SIZE);
-}
-
-int resolveCollision1(int hash, char registration[])
-{
-    char firstCharactere[2] = {registration[0], '\0'};
-    return (hash + atoi(firstCharactere));
-}
-
-int resolveCollision2(int hash, char registration[])
-{
-    (void)registration;
-    return (hash + 7);
-}
-
+// Declarações das funções principais
+void display_menu();
+void testHashFunction(int hashFunction(char[]), int resolveCollision(int, char[]));
+Employee generateEmployee();
+void initializeHashTable(HashTable *hashTable);
 int insertIntoHashTable(HashTable *hashTable, int hash, char registration[], Employee employee,
-                           int resolveCollision(int, char[]))
-{
-    int collisions = 0, inserted = 0;
-    int newHash = hash;
+                        int resolveCollision(int, char[]));
 
-    while (newHash < AVAILABLE_HASH_SIZE && !inserted)
-    {
-        if (hashTable->hash[newHash] == -1)
-        {
-            hashTable->hash[newHash] = newHash;
-            hashTable->employee[newHash] = employee;
-            inserted = 1;
-        }
-        else
-        {
-            collisions++;
-            newHash = resolveCollision(newHash, registration);
-        }
-    }
+// Declarações de funções auxiliares
+int functionHash1(char registration[]);
+int functionHash2(char registration[]);
+int resolveCollision1(int hash, char registration[]);
+int resolveCollision2(int hash, char registration[]);
+int charToInt(char c);
 
-    if (!inserted)
-    {
-        hashTable->hash[hash] = hash;
-        hashTable->employee[hash] = employee;
-    }
-
-    return (collisions);
-}
-
-void testHashFunction(int hashFunction(char[]), int resolveCollision(int, char[]))
-{
-    HashTable hashTable;
-    initializeHashTable(&hashTable);
-
-    int totalCollisions = 0;
-    clock_t total_time = 0;
-
-    for (int i = 0; i < 1000; i++)
-    {
-        Employee employee = generateEmployee();
-
-        clock_t start_time = clock();
-        int hash = hashFunction(employee.registration);
-        totalCollisions += insertIntoHashTable(&hashTable, hash, employee.registration, employee, resolveCollision);
-        clock_t end_time = clock();
-
-        total_time += (end_time - start_time);
-    }
-
-    printf("\n==============================\n");
-    printf("         RESULTADOS           \n");
-    printf("==============================\n");
-    printf(" Tempo de Execução:   %.6f segundos\n", (double)total_time / CLOCKS_PER_SEC);
-    printf(" Total de Colisões:   %d colisões\n", totalCollisions);
-    printf("==============================\n");
-}
-
-void display_menu()
-{
-    printf("\n==============================\n");
-    printf("       HASH FUNCTION MENU     \n");
-    printf("==============================\n");
-    printf(" 1. Testar Função Hash 1\n");
-    printf(" 2. Testar Função Hash 2\n");
-    printf(" 3. Sair\n");
-    printf("==============================\n");
-    printf(" Escolha uma opção: ");
-}
-
+// Função principal
 int main()
 {
     srand((unsigned)time(NULL));
@@ -193,4 +68,153 @@ int main()
     } while (option != 3);
 
     return 0;
+}
+
+// Exibição do menu
+void display_menu()
+{
+    printf("\n==============================\n");
+    printf("       HASH FUNCTION MENU     \n");
+    printf("==============================\n");
+    printf(" 1. Testar Função Hash 1\n");
+    printf(" 2. Testar Função Hash 2\n");
+    printf(" 3. Sair\n");
+    printf("==============================\n");
+    printf(" Escolha uma opção: ");
+}
+
+// Teste das funções hash
+void testHashFunction(int hashFunction(char[]), int resolveCollision(int, char[]))
+{
+    HashTable hashTable;
+    initializeHashTable(&hashTable);
+
+    int totalCollisions = 0;
+    clock_t total_time = 0;
+
+    for (int i = 0; i < 1000; i++)
+    {
+        Employee employee = generateEmployee();
+
+        clock_t start_time = clock();
+        int hash = hashFunction(employee.registration);
+        totalCollisions += insertIntoHashTable(&hashTable, hash, employee.registration, employee, resolveCollision);
+        clock_t end_time = clock();
+
+        total_time += (end_time - start_time);
+    }
+
+    printf("\n==============================\n");
+    printf("         RESULTADOS           \n");
+    printf("==============================\n");
+    printf(" Tempo de Execução:   %.6f segundos\n", (double)total_time / CLOCKS_PER_SEC);
+    printf(" Total de Colisões:   %d colisões\n", totalCollisions);
+    printf("==============================\n");
+}
+
+// Geração de um funcionário aleatório
+Employee generateEmployee()
+{
+    int number = rand() % 100000;
+    Employee employee;
+    employee.salary = (float)number;
+
+    for (int i = 0; i < REGISTRATION_SIZE; i++, number /= 10)
+    {
+        int currentDigit = number % 10;
+        employee.registration[i] = (char)currentDigit + '0';
+        employee.name[i] = (char)currentDigit + '0';
+        employee.role[i] = (char)currentDigit + '0';
+    }
+
+    employee.registration[REGISTRATION_SIZE - 1] = '\0';
+    employee.name[REGISTRATION_SIZE - 1] = '\0';
+    employee.role[REGISTRATION_SIZE - 1] = '\0';
+
+    if (employee.registration[0] == '0')
+        employee.registration[0] = '1';
+
+    return employee;
+}
+
+// Inicialização da tabela hash
+void initializeHashTable(HashTable *hashTable)
+{
+    for (int i = 0; i < AVAILABLE_HASH_SIZE; i++)
+        hashTable->hash[i] = -1;
+}
+
+// Inserção na tabela hash
+int insertIntoHashTable(HashTable *hashTable, int hash, char registration[], Employee employee,
+                        int resolveCollision(int, char[]))
+{
+    int collisions = 0, inserted = 0;
+    int newHash = hash;
+
+    while (newHash < AVAILABLE_HASH_SIZE && !inserted)
+    {
+        if (hashTable->hash[newHash] == -1)
+        {
+            hashTable->hash[newHash] = newHash;
+            hashTable->employee[newHash] = employee;
+            inserted = 1;
+        }
+        else
+        {
+            collisions++;
+            newHash = resolveCollision(newHash, registration);
+        }
+    }
+
+    if (!inserted)
+    {
+        hashTable->hash[hash] = hash;
+        hashTable->employee[hash] = employee;
+    }
+
+    return collisions;
+}
+
+// Funções hash
+int functionHash1(char registration[])
+{
+    char rearranged[REGISTRATION_SIZE];
+
+    rearranged[0] = registration[REGISTRATION_SIZE - 2];
+    rearranged[1] = registration[REGISTRATION_SIZE - 1];
+    for (int i = 0, j = 2; i < REGISTRATION_SIZE - 2; i++, j++)
+        rearranged[j] = registration[i];
+
+    int integerValue = atoi((char[]){rearranged[1], rearranged[3], rearranged[5]});
+
+    return (integerValue % AVAILABLE_HASH_SIZE);
+}
+
+int functionHash2(char registration[])
+{
+    int part1 = (charToInt(registration[0]) * 100) + (charToInt(registration[2]) * 10) + charToInt(registration[5]);
+    int part2 = (charToInt(registration[1]) * 100) + (charToInt(registration[3]) * 10) + charToInt(registration[4]);
+
+    int sumParts = part1 + part2;
+
+    return (sumParts % AVAILABLE_HASH_SIZE);
+}
+
+// Resolução de colisões
+int resolveCollision1(int hash, char registration[])
+{
+    char firstCharactere[2] = {registration[0], '\0'};
+    return (hash + atoi(firstCharactere));
+}
+
+int resolveCollision2(int hash, char registration[])
+{
+    (void)registration;
+    return (hash + 7);
+}
+
+// Conversão de char para int
+int charToInt(char c)
+{
+    return ((int)c - '0');
 }
--- Q4/150.c
+++ Q4/150.c
@@ -6,133 +6,83 @@
 #define REGISTRATION_SIZE 6
 #define HASH_TABLE_SIZE 150
 
-typedef struct Employee
-{
+typedef struct Employee {
     char registration[REGISTRATION_SIZE];
     char name[REGISTRATION_SIZE];
     char role[REGISTRATION_SIZE];
     float salary;
 } Employee;
 
-typedef struct HashTable
-{
+typedef struct HashTable {
     int keys[HASH_TABLE_SIZE];
     Employee employees[HASH_TABLE_SIZE];
 } HashTable;
 
+// High-Level Functions
+void testHashFunction(int (*hashFunction)(const char[]), int (*collisionResolver)(int, const char[]));
+int main();
 
+// Medium-Level Functions
+Employee createEmployee();
+int insertIntoHashTable(HashTable *table, int hash, const char registration[], Employee employee, int (*collisionResolver)(int, const char[]));
+void initializeHashTable(HashTable *table);
 
-Employee createEmployee()
-{
-    int number = rand() % 100000;
-    Employee employee;
-    employee.salary = (float)number;
+// Hash Functions
+int functionHash1(const char registration[]);
+int functionHash2(const char registration[]);
 
-    for (int i = 0; i < REGISTRATION_SIZE; i++, number /= 10)
-    {
-        int currentDigit = number % 10;
-        employee.registration[i] = (char)(currentDigit + '0');
-        employee.name[i] = (char)(currentDigit + '0');
-        employee.role[i] = (char)(currentDigit + '0');
-    }
+// Collision Resolvers
+int resolveCollision1(int hash, const char registration[]);
+int resolveCollision2(int hash, const char registration[]);
 
-    employee.registration[REGISTRATION_SIZE - 1] = '\0';
-    employee.name[REGISTRATION_SIZE - 1] = '\0';
-    employee.role[REGISTRATION_SIZE - 1] = '\0';
+// Utility Functions
+int charToInt(char c);
 
-    if (employee.registration[0] == '0')
-        employee.registration[0] = '1';
+// Implementation
 
-    return (employee);
+int main() {
+    srand((unsigned)time(NULL));
+    int option;
+
+    do {
+        printf("\n===========================\n");
+        printf("       Hash Tester         \n");
+        printf("===========================\n");
+        printf("1. Testar Fun\u00e7\u00e3o de Hash 1\n");
+        printf("2. Testar Fun\u00e7\u00e3o de Hash 2\n");
+        printf("3. Sair\n");
+        printf("===========================\n");
+        printf("Escolha uma op\u00e7\u00e3o: ");
+        scanf("%d", &option);
+
+        switch (option) {
+            case 1:
+                printf("\nTestando Fun\u00e7\u00e3o de Hash 1:\n");
+                testHashFunction(functionHash1, resolveCollision1);
+                break;
+            case 2:
+                printf("\nTestando Fun\u00e7\u00e3o de Hash 2:\n");
+                testHashFunction(functionHash2, resolveCollision2);
+                break;
+            case 3:
+                printf("\nSaindo do programa...\n");
+                break;
+            default:
+                printf("\nOp\u00e7\u00e3o inv\u00e1lida! Tente novamente.\n");
+        }
+    } while (option != 3);
+
+    return 0;
 }
 
-
-int insertIntoHashTable(HashTable *table, int hash, const char registration[], Employee employee, int (*collisionResolver)(int, const char[]))
-{
-  int collisions = 0, insered = 0;
-  int newHash = hash;
-
-  while(newHash < HASH_TABLE_SIZE && insered == 0)
-  {
-    if (table->keys[newHash] == -1)
-    {
-      table->keys[newHash] = newHash;
-      table->employees[newHash] = employee;
-      insered = 1;
-    }
-    else
-    {
-      collisions++;
-      newHash = collisionResolver(newHash, registration);
-    }
-  }
-
-  if (insered == 0)
-  {
-    table->keys[hash] = hash;
-    table->employees[hash] = employee;
-  }
-
-  return (collisions);
-}
-int charToInt(char c)
-{
-    return (int)(c - '0');
-}
-
-void initializeHashTable(HashTable *table)
-{
-    for (int i = 0; i < HASH_TABLE_SIZE; i++)
-        table->keys[i] = -1;
-}
-
-
-int functionHash1(const char registration[])
-{
-    char rearranged[REGISTRATION_SIZE];
-
-    rearranged[0] = registration[REGISTRATION_SIZE - 2];
-    rearranged[1] = registration[REGISTRATION_SIZE - 1];
-
-    for (int i = 0, j = 2; i < REGISTRATION_SIZE - 2; i++, j++)
-        rearranged[j] = registration[i];
-
-    int combinedValue = atoi((char[]){rearranged[1], rearranged[3], rearranged[5]});
-
-    return (combinedValue % HASH_TABLE_SIZE);
-}
-
-int functionHash2(const char registration[])
-{
-    int part1 = charToInt(registration[0]) * 100 + charToInt(registration[2]) * 10 + charToInt(registration[5]);
-    int part2 = charToInt(registration[1]) * 100 + charToInt(registration[3]) * 10 + charToInt(registration[4]);
-
-    return ((part1 + part2) % HASH_TABLE_SIZE);
-}
-
-int resolveCollision1(int hash, const char registration[])
-{
-    char firstDigit[2] = {registration[0], '\0'};
-    return (hash + atoi(firstDigit));
-}
-
-int resolveCollision2(int hash, const char registration[])
-{
-    (void)registration;
-    return (hash + 5);
-}
-
-
-void testHashFunction(int (*hashFunction)(const char[]), int (*collisionResolver)(int, const char[]))
-{
+void testHashFunction(int (*hashFunction)(const char[]), int (*collisionResolver)(int, const char[])) {
     HashTable table;
     initializeHashTable(&table);
 
     int totalCollisions = 0;
     clock_t totalTime = 0;
 
-    for (int i = 0; i < 1000; i++)
-    {
+    for (int i = 0; i < 1000; i++) {
         Employee employee = createEmployee();
 
         clock_t startTime = clock();
@@ -146,44 +96,92 @@ void testHashFunction(int (*hashFunction)(const char[]), int (*collisionResolver
     printf("\n==============================\n");
     printf("         RESULTADOS           \n");
     printf("==============================\n");
-    printf(" Tempo de Execução:   %f segundos\n", (double)totalTime / CLOCKS_PER_SEC);
-    printf(" Total de Colisões:   %d colisões\n", totalCollisions);
+    printf(" Tempo de Execu\u00e7\u00e3o:   %f segundos\n", (double)totalTime / CLOCKS_PER_SEC);
+    printf(" Total de Colis\u00f5es:   %d colis\u00f5es\n", totalCollisions);
     printf("==============================\n");
-
 }
 
-int main() {
-    srand((unsigned)time(NULL));
-    int option;
+Employee createEmployee() {
+    int number = rand() % 100000;
+    Employee employee;
+    employee.salary = (float)number;
 
-    do {
-        printf("\n===========================\n");
-        printf("       Hash Tester         \n");
-        printf("===========================\n");
-        printf("1. Testar Função de Hash 1\n");
-        printf("2. Testar Função de Hash 2\n");
-        printf("3. Sair\n");
-        printf("===========================\n");
-        printf("Escolha uma opção: ");
-        scanf("%d", &option);
+    for (int i = 0; i < REGISTRATION_SIZE; i++, number /= 10) {
+        int currentDigit = number % 10;
+        employee.registration[i] = (char)(currentDigit + '0');
+        employee.name[i] = (char)(currentDigit + '0');
+        employee.role[i] = (char)(currentDigit + '0');
+    }
 
-        switch (option) {
-            case 1:
-                printf("\nTestando Função de Hash 1:\n");
-                testHashFunction(functionHash1, resolveCollision1);
-                break;
-            case 2:
-                printf("\nTestando Função de Hash 2:\n");
-                testHashFunction(functionHash2, resolveCollision2);
-                break;
-            case 3:
-                printf("\nSaindo do programa...\n");
-                break;
-            default:
-                printf("\nOpção inválida! Tente novamente.\n");
+    employee.registration[REGISTRATION_SIZE - 1] = '\0';
+    employee.name[REGISTRATION_SIZE - 1] = '\0';
+    employee.role[REGISTRATION_SIZE - 1] = '\0';
+
+    if (employee.registration[0] == '0')
+        employee.registration[0] = '1';
+
+    return employee;
+}
+
+int insertIntoHashTable(HashTable *table, int hash, const char registration[], Employee employee, int (*collisionResolver)(int, const char[])) {
+    int collisions = 0, insered = 0;
+    int newHash = hash;
+
+    while (newHash < HASH_TABLE_SIZE && insered == 0) {
+        if (table->keys[newHash] == -1) {
+            table->keys[newHash] = newHash;
+            table->employees[newHash] = employee;
+            insered = 1;
+        } else {
+            collisions++;
+            newHash = collisionResolver(newHash, registration);
         }
-    } while (option != 3);
+    }
 
-    return 0;
+    if (insered == 0) {
+        table->keys[hash] = hash;
+        table->employees[hash] = employee;
+    }
+
+    return collisions;
 }
 
+void initializeHashTable(HashTable *table) {
+    for (int i = 0; i < HASH_TABLE_SIZE; i++)
+        table->keys[i] = -1;
+}
+
+int functionHash1(const char registration[]) {
+    char rearranged[REGISTRATION_SIZE];
+
+    rearranged[0] = registration[REGISTRATION_SIZE - 2];
+    rearranged[1] = registration[REGISTRATION_SIZE - 1];
+
+    for (int i = 0, j = 2; i < REGISTRATION_SIZE - 2; i++, j++)
+        rearranged[j] = registration[i];
+
+    int combinedValue = atoi((char[]){rearranged[1], rearranged[3], rearranged[5]});
+
+    return (combinedValue % HASH_TABLE_SIZE);
+}
+
+int functionHash2(const char registration[]) {
+    int part1 = charToInt(registration[0]) * 100 + charToInt(registration[2]) * 10 + charToInt(registration[5]);
+    int part2 = charToInt(registration[1]) * 100 + charToInt(registration[3]) * 10 + charToInt(registration[4]);
+
+    return ((part1 + part2) % HASH_TABLE_SIZE);
+}
+
+int resolveCollision1(int hash, const char registration[]) {
+    char firstDigit[2] = {registration[0], '\0'};
+    return (hash + atoi(firstDigit));
+}
+
+int resolveCollision2(int hash, const char registration[]) {
+    (void)registration;
+    return (hash + 5);
+}
+
+int charToInt(char c) {
+    return (int)(c - '0');
+}
